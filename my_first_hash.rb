325c727c345b614c7159b9a516ec8b593af7f282
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-06-25 04:42:54 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,4 @@
 def my_hash = {"dog" => "Bordeaux", "nephew" => "Daniel"}
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-
 end
 
 
